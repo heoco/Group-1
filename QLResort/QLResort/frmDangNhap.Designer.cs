11e68169a8ee8dc6aaab8c1b129e16bceba7ad5b
phonggg96 <phonggg96@gmail.com>
2017-05-18 23:28:51 +0700

    [Phong] update 3 - DangNhap + NhanVien

---
+++ QLResort/QLResort/frmDangNhap.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace QLResort
+{
+    partial class frmDangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.txtUser = new DevExpress.XtraEditors.TextEdit();
+            this.txtPass = new DevExpress.XtraEditors.TextEdit();
+            this.btnDangNhap = new DevExpress.XtraEditors.SimpleButton();
+            this.btnThoat = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.txtUser.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPass.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(428, 69);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Đăng nhập";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl1.Location = new System.Drawing.Point(79, 85);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(43, 22);
+            this.labelControl1.TabIndex = 1;
+            this.labelControl1.Text = "User:";
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.labelControl2.Appearance.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl2.Location = new System.Drawing.Point(79, 133);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(42, 22);
+            this.labelControl2.TabIndex = 1;
+            this.labelControl2.Text = "Pass:";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txtUser.Location = new System.Drawing.Point(128, 82);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Properties.Appearance.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUser.Properties.Appearance.Options.UseFont = true;
+            this.txtUser.Size = new System.Drawing.Size(222, 28);
+            this.txtUser.TabIndex = 0;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txtPass.Location = new System.Drawing.Point(128, 130);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Properties.Appearance.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPass.Properties.Appearance.Options.UseFont = true;
+            this.txtPass.Size = new System.Drawing.Size(222, 28);
+            this.txtPass.TabIndex = 1;
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnDangNhap.Appearance.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDangNhap.Appearance.ForeColor = System.Drawing.Color.Blue;
+            this.btnDangNhap.Appearance.Options.UseFont = true;
+            this.btnDangNhap.Appearance.Options.UseForeColor = true;
+            this.btnDangNhap.Location = new System.Drawing.Point(112, 179);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(82, 34);
+            this.btnDangNhap.TabIndex = 2;
+            this.btnDangNhap.Text = "Đăng nhập";
+            this.btnDangNhap.Click += new System.EventHandler(this.btnDangNhap_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnThoat.Appearance.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.Appearance.ForeColor = System.Drawing.Color.Blue;
+            this.btnThoat.Appearance.Options.UseFont = true;
+            this.btnThoat.Appearance.Options.UseForeColor = true;
+            this.btnThoat.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnThoat.Location = new System.Drawing.Point(234, 179);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(82, 34);
+            this.btnThoat.TabIndex = 3;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // frmDangNhap
+            // 
+            this.AcceptButton = this.btnDangNhap;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnThoat;
+            this.ClientSize = new System.Drawing.Size(428, 261);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnDangNhap);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(444, 300);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(444, 300);
+            this.Name = "frmDangNhap";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng nhập";
+            this.Load += new System.EventHandler(this.frmDangNhap_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.txtUser.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPass.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.TextEdit txtUser;
+        private DevExpress.XtraEditors.TextEdit txtPass;
+        private DevExpress.XtraEditors.SimpleButton btnDangNhap;
+        private DevExpress.XtraEditors.SimpleButton btnThoat;
+    }
+}
---
+++ QLResort/QLResort/frmNhanVien.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using ResortBLL;
+using ResortDTO;
+
+namespace QLResort
+{
+    public partial class frmNhanVien : Form
+    {
+        public static bool coHieu;
+        Image img;
+
+        public frmNhanVien()
+        {
+            InitializeComponent();
+        }
+
+        private void frmNhanVien_Load(object sender, EventArgs e)
+        {
+            coHieu = false;
+            img = null;
+            lblUser.Text = "NV: " + frmDangNhap.iDNhanVien + " - " + frmDangNhap.nameNhanVien;
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            gridControlNhanVien.DataSource = GetNhanVien();
+        }
+
+        private DataTable GetNhanVien()
+        {
+            string sql = "SELECT * FROM NhanVien ORDER BY TrangThai DESC";
+            try
+            {
+                return new NhanVienBLL().GetNhanVien(sql);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi: " + ex.Message, "Chú ý!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return null;
+        }
+
+        private NhanVien GetInfo()
+        {
+            try
+            {
+                int iD = Convert.ToInt32(txtID.Text);
+                string pass = txtPass.Text.Trim();
+                string ho = txtHo.Text.Trim();
+                string ten = txtTen.Text.Trim();
+                bool gioiTinh = radNam.Checked;
+                DateTime ngaySinh = Convert.ToDateTime(dateNgaySinh.EditValue);
+                string diaChi = mmoDiaChi.Text.Trim();
+                Int64 cMND = Convert.ToInt64(txtCMND.Text);
+                string sDT = txtSDT.Text.Trim();
+                string bangCap = txtBangCap.Text.Trim();
+                string chucVu = txtChucVu.Text.Trim();
+                byte[] hinhAnh = new XuLyAnh().ImageToArrayByte(img);
+                bool trangThai = toggleTrangThai.IsOn;
+
+                return new NhanVien(iD, pass, ho, ten, gioiTinh, ngaySinh, diaChi, cMND, sDT, bangCap, chucVu, hinhAnh, trangThai);
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Lỗi nhập sai định dạng: " + ex.Message, "Chú ý!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khởi tạo: " + ex.Message, "Chú ý!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return null;
+        }
+
+        private void btnChooseImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlgOpen = new OpenFileDialog();
+            dlgOpen.Filter = "All Files (*.*)|*.*|JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif";
+            if (dlgOpen.ShowDialog() == DialogResult.OK)
+            {
+                string pathImage = dlgOpen.FileName;
+                try
+                {
+                    img = Image.FromFile(pathImage);
+                    picNhanVien.Image = img;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Thông Báo!");
+                }
+            }
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            NhanVien nv = GetInfo();
+            if (nv == null)
+            {
+                return;
+            }
+            try
+            {
+                new NhanVienBLL().Add(nv);
+                RefreshInfo();
+                LoadData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi: " + ex.Message, "Chú ý!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            NhanVien nv = GetInfo();
+            if (nv == null)
+            {
+                return;
+            }
+            try
+            {
+                new NhanVienBLL().Change(nv);
+                RefreshInfo();
+                LoadData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi: " + ex.Message, "Chú ý!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtID.Text.Equals(""))
+                {
+                    throw new Exception("ID không thể trống!");
+                }
+                int iD = Convert.ToInt32(txtID.Text);
+                new NhanVienBLL().Remove(iD);
+                RefreshInfo();
+                LoadData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi: " + ex.Message, "Chú ý!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn muốn Refesh?", "Chú ý!", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                RefreshInfo();
+            }
+        }
+
+        private void RefreshInfo()
+        {
+            txtID.Text = "";
+            txtPass.Text = "";
+            txtHo.Text = "";
+            txtTen.Text = "";
+            radNam.Checked = true;
+            dateNgaySinh.Text = "";
+            mmoDiaChi.Text = "";
+            txtCMND.Text = "";
+            txtSDT.Text = "";
+            txtBangCap.Text = "";
+            txtChucVu.Text = "";
+            picNhanVien.Image = null;
+            toggleTrangThai.IsOn = true;
+        }
+
+        private void btnDong_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnDangXuat_Click(object sender, EventArgs e)
+        {
+            coHieu = true;
+            this.Close();
+        }
+
+        private void gridViewNhanVien_Click(object sender, EventArgs e)
+        {
+            if (gridViewNhanVien.SelectedRowsCount == 1)
+            {
+                txtID.Text = gridViewNhanVien.GetFocusedRowCellValue(colID).ToString();
+                txtPass.Text = "";
+                txtHo.Text = gridViewNhanVien.GetFocusedRowCellValue(colHo).ToString();
+                txtTen.Text = gridViewNhanVien.GetFocusedRowCellValue(colTen).ToString();
+                if (gridViewNhanVien.GetFocusedRowCellValue(colGioiTinh).ToString().Trim().ToLower() == "true")
+                {
+                    radNam.Checked = true;
+                }
+                else
+                {
+                    radNu.Checked = true;
+                }
+                dateNgaySinh.EditValue = gridViewNhanVien.GetFocusedRowCellValue(colNgaySinh);
+                mmoDiaChi.Text = gridViewNhanVien.GetFocusedRowCellValue(colDiaChi).ToString();
+                txtCMND.Text = gridViewNhanVien.GetFocusedRowCellValue(colCMND).ToString();
+                txtSDT.Text = gridViewNhanVien.GetFocusedRowCellValue(colSDT).ToString();
+                txtBangCap.Text = gridViewNhanVien.GetFocusedRowCellValue(colBangCap).ToString();
+                txtChucVu.Text = gridViewNhanVien.GetFocusedRowCellValue(colChucVu).ToString();
+                picNhanVien.Image = (gridViewNhanVien.GetFocusedRowCellValue(colHinhAnh).ToString() != "") ? new XuLyAnh().ArrayByteToImage((byte[])gridViewNhanVien.GetFocusedRowCellValue(colHinhAnh)) : null;
+                toggleTrangThai.EditValue = gridViewNhanVien.GetFocusedRowCellValue(colTrangThai);
+            }
+        }
+
+        private void frmNhanVien_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (coHieu)
+            {
+                return;
+            }
+            if (MessageBox.Show("Bạn muốn thoát khỏi hệ thống?", "Thoát!", MessageBoxButtons.YesNo, MessageBoxIcon.Question) != DialogResult.Yes)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
